f8e1ad4271edc0dd7b4df153b2f7a91f79168ad6
yourname <youraddress@company.com>
2024-11-18 21:28:40 +0800

    Made by Tianlang 18/11/2024

--- src/UDP_Example_create/Teacher.java
+++ src/UDP_Example_create/Teacher.java
@@ -3,6 +3,6 @@ package UDP_Example_create;
 public class Teacher {
     public static void main(String[] args) {
         new Thread(new UDPSender("127.0.0.1",7777)).start();
-        new Thread(new UDPReceiver(8888,"学生")).start();
+        new Thread(new UDPReceiver(8888,"Ybanlance")).start();
     }
 }
